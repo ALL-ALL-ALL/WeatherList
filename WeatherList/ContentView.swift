b73d8c5232377e45cc41bed7e29d766a00dba602
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-01-26 20:39:50 +0100

    Initial Commit

---
+++ WeatherList/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  WeatherList
+//
+//  Created by  Ixart on 26/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Image(systemName: "sun.rain.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherList/WeatherListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherListApp.swift
+//  WeatherList
+//
+//  Created by  Ixart on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
