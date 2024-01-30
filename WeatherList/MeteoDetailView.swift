2417b10dd7471ad425244f70f6fda8467e84575e
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-01-30 20:08:47 +0100

    Exo completer et finis

---
+++ WeatherList/MeteoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MeteoDetailView.swift
+//  WeatherList
+//
+//  Created by  Ixart on 30/01/2024.
+//
+
+import SwiftUI
+
+struct MeteoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeteoDetailView()
+}
