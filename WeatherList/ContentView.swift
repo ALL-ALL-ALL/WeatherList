7f9aa5da1913fade6a91ceaf2f27446cf08c2ef5
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-01-30 15:12:07 +0100

    j'ai rajouter le tableaux les donnés (struct ) et le systemname yeah :)

--- WeatherList/ContentView.swift
+++ WeatherList/ContentView.swift
@@ -7,28 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            List(){
-                
-            }
-            List{
-            Text("oui ")
-            Text("ou non ")
-            Text(" baba ")
-            }
-       
-
-        } // FIN VSTACK
-        
-        
-        
-        
-        
-        
-    } // FIN BODY
+struct Meteo : Identifiable {
+    
+    var id = UUID()
+    var symbolName: String
+    var temps: String
+    var ville: String
+    
 } // FIN STRUCT
+
+
+
+struct ContentView: View {
+    
+    let meteoArray: [Meteo] = [
+        
+        Meteo(id: UUID(), symbolName: "sun.horizon", temps: "Sun", ville: "Paris"),
+        Meteo(id: UUID(), symbolName: "cloud.fill", temps: "Clouds", ville: "New York"),
+        Meteo(id: UUID(), symbolName:"tropicalstorm.circle", temps: "Storm", ville:"Tokyo"),
+        Meteo(id: UUID(), symbolName: "moon.fill", temps: "Moon", ville: "Jakarta"),
+        Meteo(id: UUID(), symbolName: "snowflake", temps: "Snow", ville: "Medellin"),
+        Meteo(id: UUID(), symbolName: "tornado", temps: "Tornado", ville: "Porto")
+
+            
+        ] // FIN TABLEAU
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            List(meteoArray) { Meteo in
+                
+                  HStack {
+                                  Image(systemName: Meteo.symbolName)
+                                      .foregroundColor(.red)
+                                      .imageScale(.large)
+                                      .padding()
+                                  Text("\(Meteo.temps) \(Meteo.ville)")
+                      
+                      
+                      
+                      
+                      
+                              }  // FIN HSTACK
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                          } // FIN LIST
+            .navigationBarTitle("Weather") // Titre de la barre de navigation
+
+
+                  
+             
+
+              } // FIN NAVIGATION VIEW
+        } // FIN BODY
+  
+    
+        
+        
+        
+        
+        
+        
+    } // FIN STRUC
+    
+    
+    
+    
 
 #Preview {
     ContentView()
